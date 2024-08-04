d9a502bbb4b86c5f376fd9c1291120eba2ad43c8
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-04 15:19:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from TestColourCode import test_cases
+from GenerateReferenceManual import reference_manual_generator
+
+def main():
+  test_cases()
+  reference_manual = reference_manual_generator
 
 if __name__ == 'main':
-  test_cases()
+  main()

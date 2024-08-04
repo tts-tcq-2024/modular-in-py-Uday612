ed2fedecfd32b8b79b323a412f53c4ab0907fdaf
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-04 15:23:59 +0530

    Update GenerateReferenceManual.py

--- GenerateReferenceManual.py
+++ GenerateReferenceManual.py
@@ -1,10 +1,8 @@
-from ColourCode import MAJOR_COLORS, MINOR_COLORS, color_pair_to_string
+from ColourCode import MAJOR_COLORS, MINOR_COLORS, color_pair_to_string, get_pair_number_from_color
 def reference_manual_generator():
-    pair_number = 1
     reference ={}
     for major_color in MAJOR_COLORS:
         for minor_color in MINOR_COLORS:
-            reference[pair_number] = color_pair_to_string(major_color, minor_color)
-            pair_number +=1
+            reference[get_pair_number_from_color(major_color, minor_color)] = color_pair_to_string(major_color, minor_color)
             
     return reference 
